ccbea36012c116ba67bcd2b0149767be215817fd
bonny-art <svitlana.otenko@gmail.com>
2025-05-17 12:51:57 +0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from "./components/Container/Container";
+import Section from "./components/Section/Section";
+
+function App() {
+  return (
+    <>
+      <Section>
+        <Container>Hello</Container>
+      </Section>
+    </>
+  );
+}
+
+export default App;
